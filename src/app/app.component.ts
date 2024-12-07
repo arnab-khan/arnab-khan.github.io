af7fa3c19dae02bf0bc5b4e19b4ef003d84d0357
Arnab Khan <aranabkhan@gmail.com>
2024-12-07 21:00:07 +0530

    build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { RouterOutlet } from '@angular/router';
-import { PersonalProjectComponent } from './sub-components/personal-project/personal-project.component';
 
 @Component({
   selector: 'app-root',
